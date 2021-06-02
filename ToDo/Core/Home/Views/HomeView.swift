d89f138d414d441d12e46f8134fa41ce0a891433
Egor Syrtcov <>
2021-06-02 16:48:54 +0300

    Create header

---
+++ ToDo/Core/Home/Views/HomeView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by Egor Syrtcov on 5/31/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showDetailView: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showDetailView ? "plus" : "info")
+                .animation(.none)
+                .background(CircleButtonAnimationView(animate: $showDetailView))
+            Spacer()
+            Text(showDetailView ? "New TASK" : "ToDo List 💡")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showDetailView ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showDetailView.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
