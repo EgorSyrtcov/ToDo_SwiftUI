786b98622cce2b45750a102cd32ff55b894677d6
Egor Syrtcov <>
2021-06-04 12:33:17 +0300

    Change screen addTaskView

--- ToDo/Core/Home/Views/AddTaskView.swift
+++ ToDo/Core/Home/Views/AddTaskView.swift
@@ -13,18 +13,27 @@ struct AddTaskView: View {
     @State private var newDiscription: String = ""
     
     var body: some View {
-        VStack {
-            TextField("Please add your new task", text: $newTask)
-                .frame(height: 55)
-                .background(Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)))
-                .cornerRadius(10)
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
             
-            TextField("Discription", text: $newDiscription)
-                .frame(height: 55)
-                .background(Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)))
-                .cornerRadius(10)
+            VStack(spacing: 25) {
+                Text("Enter your new TASK")
+                    .font(.largeTitle)
+                TextField("Title task", text: $newTask)
+                    .frame(height: 55)
+                    .background(Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)))
+                    .cornerRadius(10)
+                
+                TextField("Discription", text: $newDiscription)
+                    .frame(height: 55)
+                    .background(Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)))
+                    .cornerRadius(10)
+            }
+            .padding()
         }
-        .padding(14)
+        .frame(width: 360, height: 300)
     }
 }
 
